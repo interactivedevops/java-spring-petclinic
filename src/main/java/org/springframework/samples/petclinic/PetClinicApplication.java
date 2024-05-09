c9c4b96091d660962d6198ac43b92899f002630c
Ganesh Pote <56916550+ganeshcpote@users.noreply.github.com>
2024-05-09 17:34:51 +0530

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -18,7 +18,7 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.context.annotation.ImportRuntimeHints; 
 
 /**
  * PetClinic Spring Boot Application.
